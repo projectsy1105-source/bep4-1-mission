91ac90ed9d8c66b8aaa05362b088871cbc7e17d7
박성수 <nz1245@gmail.com>
2026-09-18 11:21:30 +0900

    포스트 멤버 상속 클래스 자동생성부분삭제

---
+++ src/main/java/com/back/global/entity/BaseIdAndTimeManual.java
@@ -0,0 +1,27 @@
+package com.back.global.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseIdAndTimeManual extends BaseEntity {
+
+    @Id
+    private int id;
+
+    private LocalDateTime createDate;
+
+    private LocalDateTime modifyDate;
+}
