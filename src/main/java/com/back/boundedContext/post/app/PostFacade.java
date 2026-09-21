ad783e7846755958589033b3387fee2643accf1f
박성수 <nz1245@gmail.com>
2026-09-21 10:41:08 +0900

    PostApiClient 구현

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -32,6 +33,11 @@ public class PostFacade {
     @Transactional(readOnly = true)
     public Optional<PostMember> findPostMember(String username) {
         return postSupport.findPostMember(username);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Post> findByOrderByIdDesc() {
+        return postSupport.findByOrderByIdDesc();
     }
 
     @Transactional
--- src/main/java/com/back/boundedContext/post/app/PostSupport.java
+++ src/main/java/com/back/boundedContext/post/app/PostSupport.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -27,4 +28,9 @@ public class PostSupport {
     public Optional<PostMember> findPostMember(String username) {
         return postMemberRepository.findByUsername(username);
     }
+
+    public List<Post> findByOrderByIdDesc() {
+        return postRepository.findByOrderByIdDesc();
+    }
+
 }
