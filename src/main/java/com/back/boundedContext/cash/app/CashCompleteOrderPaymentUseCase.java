78c09feb1414d3ebc199c093a4615084a8ef1328
박성수 <nz1245@gmail.com>
2026-09-22 09:27:14 +0900

    결제처리 코드 오류수정

--- src/main/java/com/back/boundedContext/cash/app/CashCompleteOrderPaymentUseCase.java
+++ src/main/java/com/back/boundedContext/cash/app/CashCompleteOrderPaymentUseCase.java
@@ -61,7 +61,7 @@ public class CashCompleteOrderPaymentUseCase {
                             "충전은 완료했지만 %d번 주문을 결제완료처리를 하기에는 예치금이 부족합니다.".formatted(event.getOrderDto().getId()),
                             event.getOrderDto(),
                             event.getPgPaymentAmount(),
-                            event.getPgPaymentAmount().subtract(customerWallet.getBalance())
+                            event.getOrderDto().getSalePrice().subtract(customerWallet.getBalance())
                     )
             );
         }
